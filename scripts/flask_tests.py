b759b61a165783dcaa4889d013bddf55efff1182
Marisa Smith <mesmith027@gmail.com>
2020-06-03 10:38:16 -0400

    took relevant code from QL_wine notebook and created a .py file, added webApp and template for what the web app will look like

--- scripts/flask_tests.py
+++ scripts/flask_tests.py
@@ -4,10 +4,25 @@ app = Flask(__name__)
 #to execute in debug mode (dont have to reload every time there is a change)
     # run export FLASK_DEBUG=1
 
+posts =[
+    {
+        'author': 'M.E.Smith',
+        'title': 'Blog 1',
+        'content': 'First post content',
+        'date_posted': 'June 3 2020'
+    },
+    {
+        'author': 'J. Doe',
+        'title': 'Blog 2',
+        'content': 'second post content',
+        'date_posted': 'June 4 2020'
+    }
+]
+
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template('home_test.html')
+    return render_template('home_test.html', posts=posts)
 
 @app.route("/about")
 def about():
